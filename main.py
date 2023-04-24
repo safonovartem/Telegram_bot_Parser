fb0cf3c7af68dc8a6f49d4dc19c290a9d1cbd0c4
safonovartem <safonovartem829@gmail.com>
2023-04-24 19:34:52 +0300

    Create project

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as b
+
+URL = 'https://www.afisha.ru/tver/events/na-segodnya/'
+def parser(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')#Парсинг
+    Text_for_films = soup.find_all('div', class_ = "mQ7Bh")
+    return [c.text for c in Text_for_films]
+
+list_of_jokes = parser(URL)
+print(list_of_jokes)
+trach = list_of_jokes.pop(0)
+print(list_of_jokes)
