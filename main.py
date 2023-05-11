e667b55e2c8ddb0092d5301703ad578186c0c743
safonovartem <safonovartem829@gmail.com>
2023-05-11 19:27:33 +0300

    Change project

--- main.py
+++ main.py
@@ -1,92 +1,21 @@
 import requests
-import os
-from tqdm import tqdm
-from bs4 import BeautifulSoup as bs
-from urllib.parse import urljoin, urlparse
-url = 'https://www.afisha.ru/tver/events/na-segodnya/'
+from bs4 import BeautifulSoup as b
 
-def is_valid(url):
-    """
-    Проверяем, является ли url действительным URL
-    """
-    parsed = urlparse(url)
-    return bool(parsed.netloc) and bool(parsed.scheme)
+URL = "https://www.afisha.ru/tver/events/movies/"
+def parser(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')  # Парсинг
+    Text_for_films = soup.find_all('div', class_="mQ7Bh")
+    return [c.text for c in Text_for_films]
 
+Text_for_films = parser(URL)
+Trach_words = ['События', 'Кино', 'Театр', 'Концерты', 'Дети', 'Об «Афише»', 'О нас', 'Проекты', 'Еще', '«Афиша» в соц. сетях', 'Мобильное приложение «Афиши» — самый удобный способ выбрать, как провести свободное время', 'Рассылка «Афиши»: главные события недели — у вас на почте']
 
-def get_all_images(url):
-    """
-    Возвращает все URL‑адреса изображений по одному `url`
-    """
-    soup = bs(requests.get(url).content, "html.parser")
-    urls = []
-    for img in tqdm(soup.find_all("img"), "Получено изображение"):
-        img_url = img.attrs.get("src")
-        if not img_url:
-            # если img не содержит атрибута src, просто пропускаем
-            continue
-        # сделаем URL абсолютным, присоединив имя домена к только что извлеченному URL
-        img_url = urljoin(url, img_url)
-        # удалим URL‑адреса типа '/hsts-pixel.gif?c=3.2.5'
-        try:
-            pos = img_url.index("?")
-            img_url = img_url[:pos]
-        except ValueError:
-            pass
-        # наконец, если URL действителен
-        if is_valid(img_url):
-            urls.append(img_url)
-    return urls
+def remove_common(Text_for_films, Trach_words):
+    for i in Text_for_films[:]:
+        if i in Trach_words:
+            Text_for_films.remove(i)
+            Trach_words.remove(i)
 
-
-def download(url, pathname):
-    """
-    Загружает файл по URL‑адресу и помещает его в папку `pathname`
-    """
-    # если путь не существует, создать dir
-    if not os.path.isdir(pathname):
-        os.makedirs(pathname)
-    # загружаем тело ответа по частям, а не сразу
-    response = requests.get(url, stream=True)
-
-    # получить общий размер файла
-    file_size = int(response.headers.get("Content-Length", 0))
-
-    # получаем имя файла
-    filename = os.path.join(pathname, url.split("/")[-1])
-
-    # индикатор выполнения, изменение единицы измерения на байты вместо итераций (по умолчанию tqdm)
-    progress = tqdm(response.iter_content(1024), f"Загружен {filename}", total=file_size, unit="B", unit_scale=True,
-                    unit_divisor=1024)
-    with open(filename, "wb") as f:
-        for data in progress.iterable:
-            # записываем прочитанные данные в файл
-            f.write(data)
-            # обновление индикатора выполнения вручную
-            progress.update(len(data))
-
-
-def main(url, path):
-    # получить все изображения
-    imgs = get_all_images(url)
-    for img in imgs:
-        # скачать для каждого img
-        download(img, path)
-
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Этот скрипт загружает все изображения с веб‑страницы.")
-    parser.add_argument("url", help="URL‑адрес веб‑страницы, с которой вы хотите загрузить изображения.")
-    parser.add_argument("-p", "--path",
-                        help="Каталог, в котором вы хотите хранить изображения, по умолчанию - это домен переданного URL.")
-
-    args = parser.parse_args()
-    url = args.url
-    path = args.path
-
-    if not path:
-        # если путь не указан, используйте доменное имя этого url в качестве имени папки
-        path = urlparse(url).netloc
-
-    main(url, path)
+print("list1 : ", Text_for_films)
+print("list2 : ", Trach_words)
