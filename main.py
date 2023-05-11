e89db6d93d5e6197dae91636b66ff73f470ce2ad
safonovartem <safonovartem829@gmail.com>
2023-05-11 19:44:26 +0300

    Change project

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ def parser(url):
 Text_for_films = parser(URL)
 Trach_words = ['События', 'Кино', 'Театр', 'Концерты', 'Дети', 'Об «Афише»', 'О нас', 'Проекты', 'Еще', '«Афиша» в соц. сетях', 'Мобильное приложение «Афиши» — самый удобный способ выбрать, как провести свободное время', 'Рассылка «Афиши»: главные события недели — у вас на почте']
 
-def remove_common(Text_for_films, Trach_words):
-    for i in Text_for_films[:]:
-        if i in Trach_words:
-            Text_for_films.remove(i)
-            Trach_words.remove(i)
+def remove_common(Text_for_films):
+    for i in range(len(Text_for_films)):
+        for j in range(len(Trach_words)):
+            if j in Text_for_films:
+                Text_for_films.remove(j)
+
 
 print("list1 : ", Text_for_films)
 print("list2 : ", Trach_words)
+
+#def remove(Text_for_films, Trach_words):
+    #r = []
+    #for char in range(len(Text_for_films)):
+        #if Text_for_films[char] != Trach_words[char]:
+             #r.append(Text_for_films[char])
+    #return r
+#print(Text_for_films)
