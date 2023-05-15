0050ceac25b8dea584b9fb81bbb6250b7f492dd5
safonovartem <safonovartem829@gmail.com>
2023-05-15 19:26:06 +0300

    Change project

--- main.py
+++ main.py
@@ -9,23 +9,18 @@ def parser(url):
     return [c.text for c in Text_for_films]
 
 Text_for_films = parser(URL)
-Test_spisok = ['Мама пошла в театр']
 Trach_words = ['События', 'Кино', 'Театр', 'Концерты', 'Дети', 'Об «Афише»', 'О нас', 'Проекты', 'Еще', '«Афиша» в соц. сетях', 'Мобильное приложение «Афиши» — самый удобный способ выбрать, как провести свободное время', 'Рассылка «Афиши»: главные события недели — у вас на почте']
+a = Text_for_films
+b = Trach_words
+print(a)
+print(b)
+def remove_common(a, b):
+    for i in a[:]:
+        if i in b:
+            a.remove(i)
+            b.remove(i)
+    print("list1 : ", a)
+    print("list2 : ", b)
 
-def remove_common(Test_spisok, Trach_words):
-    for i in range(len(Test_spisok)):
-        for j in range(len(Trach_words)):
-            if j in Test_spisok:
-                Test_spisok.remove(j)
 
-
-print("list1 : ", Test_spisok)
-print("list2 : ", Trach_words)
-
-#def remove(Text_for_films, Trach_words):
-    #r = []
-    #for char in range(len(Text_for_films)):
-        #if Text_for_films[char] != Trach_words[char]:
-             #r.append(Text_for_films[char])
-    #return r
-#print(Text_for_films)
+c = remove_common(a,b)
